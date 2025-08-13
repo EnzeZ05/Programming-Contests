074acea903d5b2c2104a04d2d917bc8fb00a02bb
Enze Zhang <ezhang62@wisc.edu>
2025-08-13 18:53:51 -0500

    Create d.cpp

---
+++ SHPC2025/d.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll  = long long;
+using ld  = long double;
+using i128 = __int128_t;
+
+const double  pi  = 3.14159265358979323846;
+const int mod = (int)1e9 + 7;
+const ll  INF = 1e18;
+
+template <typename T>
+T chmax(T a, T b){
+    return a > b ? a : b;
+}
+
+template <typename T>
+T chmin(T a, T b){
+    return a > b ? b : a;
+}
+
+const int N = 1e6 + 1, M = 2 * N;
+ 
+void solve(){
+    ll a, b, x, y;
+    cin >> a >> b >> x >> y;
+
+    if(a == x && b == y){
+        cout << "Yes" << endl;
+    }
+    else if(a == x && (b & y) == b){
+        cout << "Yes" << endl;
+    }
+    else if(b == y && (a & x) == x){
+        cout << "Yes" << endl;
+    }
+    else{
+        cout << "No" << endl; 
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+}
