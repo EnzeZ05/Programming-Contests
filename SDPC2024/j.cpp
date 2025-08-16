7d55a7a73ab08d5b770297f27a7cb06eeb79f191
Enze Zhang <ezhang62@wisc.edu>
2025-08-15 20:19:45 -0500

    Create j.cpp

---
+++ SDPC2024/j.cpp
@@ -0,0 +1,145 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll  = long long;
+using ld  = long double;
+using i128 = __int128_t;
+
+const double pi  = 3.14159265358979323846;
+const int mod = 998244353;
+const ll INF = 1e18;
+
+template <typename T>
+T chmax(T a, T b){
+    return a > b ? a : b;
+} 
+
+template <typename T>
+T chmin(T a, T b){
+    return a > b ? b : a;
+}
+
+const int N = 1e5 + 10, M = 2 * N;
+
+struct DSU{
+    vector<int> node, size;
+ 
+    DSU(){}
+    DSU(int n){
+        init(n);
+    }
+ 
+    void init(int n){
+        node.resize(n);
+        size.resize(n);
+ 
+        for(int i = 0; i < n; i++){
+            node[i] = i;
+            size[i] = 1;
+        }
+    }
+ 
+    int find(int x){
+        return x == node[x] ? x : node[x] = find(node[x]);
+    }
+ 
+    void erase(int x){
+        size[find(x)] -= 1;
+        node[x] = x;
+    }
+ 
+    void relocate(int x, int y){ //can't be root
+        int dx = find(x), dy = find(y);
+        if(dx == dy){
+            return;
+        }
+        node[x] = dy;
+        size[dx] -= 1;
+        size[dy] += 1;
+    }
+ 
+    int set(int x, int y){
+        return find(x) == find(y);
+    }
+ 
+    int merge(int x, int y){
+        x = find(x);
+        y = find(y);
+        if(x == y){
+            return 0;
+        }
+ 
+        size[x] += size[y];
+        node[y] = x;
+        return 1;
+    }
+ 
+    int heuristic_merge(int x, int y){ 
+        x = find(x);
+        y = find(y);
+        if(size[x] < size[y]){
+            swap(x, y);
+        }
+        return merge(x, y);
+    }
+ 
+    int get(int x){
+        return size[find(x)];
+    }
+};
+
+void solve(){
+    int n;
+    cin >> n;
+
+    ll a[n], b[n][n], c[n];
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+        c[i] = (int)1e9;
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> b[i][j];
+            c[i] = chmin(c[i], b[i][j]);
+        }
+    }
+
+    DSU dsu(n + 1);
+    vector<vector<ll>> e;
+    ll ans = 0;
+
+    for(int i = 0; i < n; i++){
+        if(a[i] > 1){
+            ans += (a[i] - 1) * c[i];
+        }
+        for(int j = 0; j < n; j++){
+            e.push_back({b[i][j], i, j});
+        }
+    }
+
+    sort(e.begin(), e.end());
+    for(int i = 0; i < e.size(); i++){
+        int u = e[i][1], v = e[i][2], w = e[i][0];
+        if(!dsu.set(u, v)){
+            dsu.heuristic_merge(u, v);
+            ans += w;
+        }
+    }
+
+    cout << ans << "\n";
+}   
+    
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    cin >> t;
+
+    while(t--){
+        solve();
+    }
+    return 0;
+}
