98021bae6f2f4912cce2a444f9f5a2864c5a9539
Enze Zhang <ezhang62@wisc.edu>
2025-08-24 22:49:53 -0500

    Create m.cpp

---
+++ ZJPC2021/m.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll  = long long;
+using ld  = long double;
+using i128 = __int128_t;
+
+const double pi  = 3.14159265358979323846;
+const int mod = 998244353;
+const ll INF = 1e18;
+
+template <typename T>
+T chmax(T a, T b){
+    return a > b ? a : b;
+} 
+
+template <typename T>
+T chmin(T a, T b){
+    return a > b ? b : a;
+}
+
+const int N = 1e5 + 10, M = 2 * N;
+
+void solve(){
+    int n;
+    cin >> n;
+    cout << 0 << endl;
+}
+    
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int t = 1;
+    // cin >> t;
+
+    while(t--){
+        solve();
+    }
+    return 0;
+}
